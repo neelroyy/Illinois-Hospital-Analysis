71fc20f8f14405a7b24e3add3a87b6d5aa4ad165
Jenna <jorstadjenna@gmail.com>
2022-06-23 21:56:00 -0500

    app.py

--- app.py
+++ app.py
@@ -19,10 +19,64 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 #Save Reference to the table
-Passenger = Base.classes.passenger
+Hospital = Base.classes.hospital
 
 ##################################################################
 # Flask Setup
 ##################################################################
 app = Flask(__name__)
+
+
+@app.route("/")
+#define what to do when user hits the index route
+def welcome():
+    return render_template('index.html')
+
+
+@app.route("/api/v1.0/name")
+def name():
+    #create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #query all hospital names
+    results = session.query(Hospital.name).all()
+
+    session.close()
+
+    #convert list of tuples into normal list
+    hospital_names = list(np.ravel(results))
+
+    return jsonify(hospital_names) 
+
+
+@app.route("/api/v1.0/city")
+def city():
+    #create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #query all cities
+    results = session.query(Hospital.city).all()
+
+    session.close()
+
+    #convert list of tuples into a normal list
+    city = list(np.ravel(results))
+
+    return jsonify(city) 
+
+
+@app.route("/api/v1.0/rating")
+def rating ():
+    #create our session (link) from Python to the DB
+    session = Session(engine)
+
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names) 
+
+if __name__ == '__main__':
+    app.run(debug = True)      
+
+
+
 
